d38e0e71f7e98a07f6bbca533172bab1b974ce2e
subin114 <bin7851@naver.com>
2024-03-08 19:21:58 +0900

    초기 설정 및 UI 구현

---
+++ section06/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Viewer from "./components/Viewer";
+import Controller from "./components/Controller";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
+  );
+}
+
+export default App;
