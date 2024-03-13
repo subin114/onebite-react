a0811302ee935a06d9bd3f5df18c3cc8aa8bc523
subin114 <bin7851@naver.com>
2024-03-13 17:21:58 +0900

    Create - todo 추가하기

--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,21 +1,35 @@
 import "./Editor.scss";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const Editor = ({ onCreate }) => {
   const [content, setContent] = useState("");
+  const contentRef = useRef();
 
   const onChangeContent = (e) => {
     setContent(e.target.value);
   };
 
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
   const onSubmit = () => {
-    onCreate();
+    if (content === "") {
+      content.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
   };
 
   return (
     <div className="Editor">
       <input
+        ref={contentRef}
         value={content}
+        onKeyDown={onKeyDown}
         onChange={onChangeContent}
         placeholder="새로운 Todo..."
       />
