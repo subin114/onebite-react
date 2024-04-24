fefe8ef734a87c2ccc2c2869e340648d4ebe791b
subin114 <bin7851@naver.com>
2024-04-24 22:40:27 +0900

    New UI 구현

---
+++ section12/src/components/EmotionItem.jsx
@@ -0,0 +1,17 @@
+import "./EmotionItem.scss";
+import { getEmotionImage } from "./../util/get-emotion-image";
+
+const EmotionItem = ({ emotionId, emotionName, isSelected }) => {
+  return (
+    <div
+      className={`EmotionItem ${
+        isSelected ? `EmotionItem_on_${emotionId}` : ""
+      }`}
+    >
+      <img className="emotion_img" src={getEmotionImage(emotionId)} />
+      <div className="emotion_name">{emotionName}</div>
+    </div>
+  );
+};
+
+export default EmotionItem;
--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,5 +1,20 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
+
 const New = () => {
-  return <div>New</div>;
+  const nav = useNavigate();
+
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default New;
