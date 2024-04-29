110d36d7fe8b62dd898f725155518ad7f4af5b90
subin114 <bin7851@naver.com>
2024-04-29 22:36:02 +0900

    New 기능 구현

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -2,9 +2,17 @@ import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext);
   const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+  };
 
   return (
     <div>
@@ -12,7 +20,7 @@ const New = () => {
         title={"새 일기 쓰기"}
         leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
       />
-      <Editor />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
