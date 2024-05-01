63e9989734c8eab4e0b523e7c3e71333a75b60bd
subin114 <bin7851@naver.com>
2024-05-01 19:33:41 +0900

    감정 일기장 최종 (배포)

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { DiaryStateContext } from "../App";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
+import usePageTitle from "../hooks/usePageTitle";
 
 const getMonthlyData = (pivotDate, data) => {
   // 이번 달 시작 시간 (1일 0시 0분 0초)
@@ -33,6 +34,7 @@ const getMonthlyData = (pivotDate, data) => {
 const Home = () => {
   const data = useContext(DiaryStateContext);
   const [pivotDate, setPivotDate] = useState(new Date()); // 날짜 보관
+  usePageTitle("감정 일기장");
 
   const monthlyData = getMonthlyData(pivotDate, data);
 
